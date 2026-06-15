73bf79131d39d8607c9dd00aafebdb4d3adfbb5f
augusto hansel schaefer dos santos <augustohansel@hotmail.com>
2026-06-15 14:47:01 -0300

    create a slider to change the grid size

--- javascript.js
+++ javascript.js
@@ -1,17 +1,29 @@
+const gridContainer = document.getElementsByClassName("grid-container")[0];
+const sketchSize = document.getElementById("sketchSize");
+const sketchSizeText = document.getElementById("sketchSizeText")
 
-const grid = 32;
-const gridContainer = document.getElementsByClassName("grid-container")[0]
+function createGrid(size) {
+    gridContainer.innerHTML = "";
 
-gridContainer.style.gridTemplateColumns = `repeat(${grid}, 1fr)`;
-gridContainer.style.gridTemplateRows = `repeat(${grid}, 1fr)`;
+    gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-for (let i = 0; i < grid * grid; i++) {
-    const cell = document.createElement("div");
+    for (let i = 0; i < size * size; i++) {
+        const cell = document.createElement("div");
 
-    cell.style.border = "1px solid black";
-    cell.addEventListener("mouseover", () => {
-        cell.style.backgroundColor = "#0000ff";
-    });
+        cell.style.border = "1px solid black";
 
-    gridContainer.appendChild(cell);
+        cell.addEventListener("mouseover", () => {
+            cell.style.backgroundColor = "#0000ff";
+        });
+
+        gridContainer.appendChild(cell);
+    }
 }
+
+createGrid(Number(sketchSize.value));
+
+sketchSize.addEventListener("input", (e) => {
+    createGrid(Number(e.target.value));
+    sketchSizeText.textContent = `${e.target.value} X ${e.target.value}`
+});
