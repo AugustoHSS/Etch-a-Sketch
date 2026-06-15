8eac2031e417057f998ab669c01a1c4f51483f61
augusto hansel schaefer dos santos <augustohansel@hotmail.com>
2026-06-15 12:46:19 -0300

    create a blank 16x16 grid

--- javascript.js
+++ javascript.js
@@ -1,1 +1,20 @@
-console.log("test")
+
+const grid = 16;
+const gridContainer = document.getElementsByClassName("grid-container")[0]
+
+for (let y = 0; y < grid; y++) {
+    const row = document.createElement("div");
+    row.style.display = "flex";
+
+    for (let x = 0; x < grid; x++) {
+        const cell = document.createElement("div");
+
+        cell.style.width = "40px";
+        cell.style.height = "40px";
+        cell.style.border = "1px solid black";
+        cell.style.cursor = "pointer"
+        row.appendChild(cell);
+    }
+
+    gridContainer.appendChild(row);
+}
